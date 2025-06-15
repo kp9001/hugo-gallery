2d1047c7a5065929bcc55445649e2869509d4c12
Kendra Plante <kendra@kplante.com>
2025-06-15 01:37:01 -0400

    add theme as submodule, test pull images script and album page

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kp9001/hugo-gallery
 
 go 1.24.4
+
+require github.com/nicokaiser/hugo-theme-gallery/v4 v4.9.0 // indirect
---
+++ pull-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Test
+wget --no-clobber "https://live.staticflickr.com/65535/54563271558_cbcf06e936_h.jpg" -O "content/test/test.jpg"
