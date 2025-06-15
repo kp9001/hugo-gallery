68ef0764200dda95a8f596b70bdc318bb8454bcf
Kendra Plante <kendra@kplante.com>
2025-06-15 18:23:54 -0400

    update build script for dynamic image pulls by album

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+key="83ba6fa46633ca5ea36c6e852c32a1d5"
+albums=("Street" "Wildlife" "Pets" "Portraits" "Motorsports")
+
+for i in ${albums[@]}; do mkdir content/$i; done
+
+for i in ${albums[@]}; do 
+	curl "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=$key&photoset_id=$(curl "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=$key&user_id=202977498@N08" 2>/dev/null | grep -e "photoset id" -e "title" | sed 's/.*photoset\ id=\"//g' | sed 's/\".*//g' | grep "$i" -B 1 | head -1)&user_id=202977498@N08&extras=url_o" 2>/dev/null | sed 's/.*url_o=\"//g' | sed 's/\".*//g' | grep .jpg$ | while read j; do 
+		wget --no-clobber $j -O "content/$i/$(curl "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=$key&photo_id=$(echo $j | cut -d/ -f5 | cut -d_ -f1)" 2>/dev/null | grep title | sed 's/.*<title>//g' | sed 's/<\/title.*//g').jpg";
+	done;
+done
+
 rm ./*.tar.gz 2>/dev/null
 
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep "extended" | grep -v "withdeploy" | cut -d\" -f4 )
